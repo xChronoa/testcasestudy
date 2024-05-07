8f7a1c1ac7d2088657fbecae26bd9ce1c468a71b
Darrel <barnedodarrel11@gmail.com>
2024-05-07 18:30:28 +0800

    viewcart

--- react-app/src/components/ViewCart.js
+++ react-app/src/components/ViewCart.js
@@ -1,35 +1,56 @@
 import React from "react";
+import { useCart } from "./utils";
+import { Link } from "react-router-dom";
+import Loading from "./Loading";
+import { ToastContainer, toast, Slide } from "react-toastify";
 
 // View Cart Component
-const ViewCart = (props) => {
-    const totalPrice = props.cart.reduce((total, item) => {
-        return total + item.price * item.quantity;
-    }, 0);
-    const totalQuantity = props.cart.reduce((totalQuantity, item) => {
-        return totalQuantity + item.quantity;
-    }, 0);
+const ViewCart = () => {
+    const { cart, loading, cartTotal, removeFromCart } = useCart();
+
+    const notify = () =>
+        toast.success("Sucessfully removed product from cart.", {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: false,
+            draggable: false,
+            progress: undefined,
+            theme: "light",
+            transition: Slide,
+        });
+    
+    // To fetch data before rendering page.
+    if (loading) {
+        return <Loading />
+    }
 
     return (
         <div className="cart-summary d-flex justify-content-start flex-column m-5 min-vh-100">
+            <ToastContainer />
             <h4>Cart Summary</h4>
             
-            {props.cart.length > 0 ? (
+            {cart.data.length > 0 ? (
                 <ul className="list-group">
                     <li className="d-flex justify-content-between align-items-center list-group-item rounded-top">
                         <span className="flex-fill col-6">Product</span>
                         <span className="flex-fill text-center col-2">Price</span>
                         <span className="flex-fill text-center col-4">Quantity</span>
-                        <div class="filler-header"></div>
+                        <div className="filler-header"></div>
                     </li>
-                    {props.cart.map((item, index) => (
-                        <li key={index} className="d-flex justify-content-between align-items-center list-group-item ">
+                    {cart.data.map((item, index) => (
+                        <li key={index} className="d-flex justify-content-between align-items-center list-group-item">
                             <span className="flex-fill product-name col-6">{item.name}</span>
                             <span className="flex-fill product-price text-center col-2"> 
                                 ₱ {item.price.toLocaleString('en-PH', {maximumFractionDigits: 2, minimumFractionDigits: 2})} 
                             </span>
                             <span className="flex-fill product-quantity text-center col-4"> {item.quantity}</span>
                             <button
-                                onClick={() => props.removeFromCart(item.id)}
+                                onClick={() => { 
+                                    removeFromCart(item.id);
+                                    notify();
+                                }}
                                 className="flex-fill btn btn-outline-danger"
                             >
                                 Remove
@@ -38,12 +59,16 @@ const ViewCart = (props) => {
                     ))}
                     <div className="price-summary d-flex flex-row justify-content-between mt-4">
                         <h4 className="fs-5"> 
-                            Total Price: ₱ {totalPrice.toLocaleString('en-PH', {maximumFractionDigits: 2, minimumFractionDigits: 2})} 
+                            Total Price: ₱ {cartTotal.totalPrice.toLocaleString('en-PH', {maximumFractionDigits: 2, minimumFractionDigits: 2})} 
                         </h4>
-                        <h4 className="fs-5"> Total Products: {totalQuantity} </h4>
+                        <h4 className="fs-5"> Total Products: {cartTotal.totalQuantity} </h4>
                     </div>
                     <div className="checkout-control d-flex flex-row justify-content-end my-3">
-                        <button className="btnCheckout btn btn-success w-100 fs-5 fw-bold" onClick={() => props.checkOut()}>Check Out</button>
+                        <button className="btnCheckout btn btn-success w-100 fs-5 fw-bold">
+                            <Link to="/checkout" className="text-white fw-bold text-decoration-none p-2">
+                                Continue to Checkout
+                            </Link>
+                        </button>
                     </div>
                 </ul>
             ) : (
@@ -52,7 +77,7 @@ const ViewCart = (props) => {
                         <span className="flex-fill col-6">Product</span>
                         <span className="flex-fill text-center col-2">Price</span>
                         <span className="flex-fill text-center col-4">Quantity</span>
-                        <div class="filler-header"></div>
+                        <div className="filler-header"></div>
                     </li>
                     <li className="text-center list-group-item">
                         There are currently no items in the cart.
