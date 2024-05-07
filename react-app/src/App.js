b6f0690bd893b610f31d35ee54b8e3e29327abdb
Darrel <barnedodarrel11@gmail.com>
2024-05-07 18:27:56 +0800

    app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useCart } from "./components/utils";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Product from "./components/Product";
 import ViewCart from "./components/ViewCart";
+import CheckOut from "./components/CheckOut";
 
 function App() {
-    const { cart, addToCart, removeFromCart, checkOut } = useCart();
-
     return (
         <div className="App">
             <BrowserRouter>
@@ -16,21 +14,9 @@ function App() {
                 <Routes>
                     <Route exact path="/" element={<Home />} />
                     <Route exact path="/home" element={<Home />} />
-                    <Route
-                        exact
-                        path="/product"
-                        element={<Product cart={cart} addToCart={addToCart} />}
-                    />
-                    <Route
-                        path="/viewcart"
-                        element={
-                            <ViewCart
-                                cart={cart}
-                                removeFromCart={removeFromCart}
-                                checkOut={checkOut}
-                            />
-                        }
-                    />
+                    <Route exact path="/product" element={<Product />} />
+                    <Route path="/viewcart" element={<ViewCart />} />
+                    <Route path="/checkout" element={<CheckOut />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
